a7d49b007f92a560bfaa757b58dc6ca7f40ba0e6
kaylamarc <kkcoons@aol.com>
2021-04-02 00:19:50 -0400

    Updated stylizing

--- repeter/src/App.js
+++ repeter/src/App.js
@@ -22,39 +22,41 @@ class App extends Component {
 
     return (
       <div className="App">
-      <header className="App-header">
-        <h1>Welcome to RePeter!</h1>
-      </header>
-      <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">
+          <h1>Welcome to RePeter!</h1>
+        </header>
+        <img src={logo} className="App-logo" alt="logo" />
 
-      <div className="Pete">
-        <h1>I am Peter! I will Re-Pete anything you want me to play! (Haha get it?)</h1>
-      </div>
-
-      <div className="Instruct">
-        <h1>Just enter a valid URL and enjoy!</h1>
-      </div>
-
-      <div className="Player">
-        <div className="Controls">
-          <input ref={input => { this.urlInput = input}} type='text' placeholder='Enter URL'></input>
-          <button className="Button" onClick={ () => this.setState({url: this.urlInput.value})}>Go!</button>
+        <div className="Pete">
+          <h1><em>"I am Peter! I will Re-Pete anything you want me to play! (Haha get it?)"</em></h1>
         </div>
-        <ReactPlayer className='React-player' url={url} loop={loop} playing={playing}></ReactPlayer>
-      </div>
-      
-      <div className="Pre-List">
-        <h1>What can I play?</h1>
-        <ul className="List">
-          <li>YouTube</li>
-          <li>Soundcloud</li>
-          <li>Twitch</li>
-          <li>Vimeo</li>
-          <li>Facebook videos</li>
-        </ul>
-      </div>
 
-    </div>
+        <div className="Instruct">
+          <h1>Just enter a valid URL and enjoy!</h1>
+        </div>
+
+        <div className="Player">
+          <div className="Controls">
+            <input ref={input => { this.urlInput = input }} type='text' placeholder='Enter URL'></input>
+            <button className="Button" onClick={() => this.setState({ url: this.urlInput.value })}>Go!</button>
+          </div>
+          <ReactPlayer className='React-player' url={url} loop={loop} playing={playing}></ReactPlayer>
+          <h1 className="Note"><em>**NOTE: If no video pops up and plays in the video above, make sure you enter a valid URL.**</em></h1>
+        </div>
+
+
+        <div className="Pre-List">
+          <h1>What can I play?</h1>
+          <ul className="List">
+            <li>YouTube</li>
+            <li>Soundcloud</li>
+            <li>Twitch</li>
+            <li>Vimeo</li>
+            <li>Facebook videos</li>
+          </ul>
+        </div>
+
+      </div>
     )
   }
 }
